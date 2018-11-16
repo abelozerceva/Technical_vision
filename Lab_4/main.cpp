3e5e20b4c6cb9882b36e03cb2fa45c8703ef804d
belozercevaa <belozerceva.a@yandex.ru>
2018-11-16 21:16:34 +0300

    Попытка сделать бабочку(успешная, но не очень)

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -17,17 +17,19 @@ using namespace std;
 Mat img;
 Mat magI;
 Mat img_fourier;
+Mat img_fast_fourier;
 
 Mat DFT(Mat &img);
 Mat myFourier(Mat &img);
 Mat beautiful_spectre(Mat &img);
 void correct_work(Mat &img1, Mat &img2);
+Mat fast_fourier(Mat &img);
 
 int main()
 {
     clock_t begin, end, time_DFT, time_myFourier;
 
-    img = imread("/home/anastasia/Рабочий стол/Lab_4/mono.jpeg", 0);
+    img = imread("/home/anastasia/git/Lab_4/mono.jpeg", 0);
 
     if(! img.data )
     {
@@ -55,13 +57,18 @@ int main()
 
     correct_work(magI, img_fourier);
 
+    fast_fourier(img);
+  //  correct_work(img_fast_fourier, img_fourier);
+
     namedWindow("Input Image", WINDOW_AUTOSIZE);
     namedWindow("Discrete Fourier Trasform", WINDOW_AUTOSIZE);
     namedWindow("My Fourier Transform", WINDOW_AUTOSIZE);
+    namedWindow("Buterfly", WINDOW_AUTOSIZE);
 
     imshow("Input Image", img);
     imshow("Discrete Fourier Trasform", magI);
     imshow("My Fourier Transform", img_fourier);
+    imshow("Buterfly", img_fast_fourier);
 
     waitKey();
     destroyAllWindows();
@@ -95,7 +102,6 @@ Mat DFT(Mat &img)
 
     // rearrange the quadrants of Fourier image  so that the origin is at the image center
     beautiful_spectre(magI);
-
     return magI;
 }
 
@@ -119,7 +125,6 @@ Mat myFourier(Mat &img)
         sum_cols[i] = new complex<double> [cols];
         sum_rows[i] = new complex<double> [cols];
         sum[i] = new double [cols];
-
         for(j=0; j<cols; j++)
         {
             sum_cols[i][j] = 0;
@@ -127,7 +132,6 @@ Mat myFourier(Mat &img)
             sum[i][j] = 0;
         }
     }
-
     for(i=0; i<rows; i++)
     {
         for(j=0; j<cols; j++)
@@ -150,9 +154,7 @@ Mat myFourier(Mat &img)
             img_fourier.at<double>(j,i) = log(sum[j][i]+1);
         }
     }
-
     beautiful_spectre(img_fourier);
-
     return img_fourier;
 }
 
@@ -180,6 +182,91 @@ Mat beautiful_spectre(Mat &img)
     return img;
 }
 
+Mat fast_fourier(Mat &img)
+{
+    int i=0, j=0, N=0;
+    double pi = 3.1416;
+    int rows = getOptimalDFTSize( img.rows )-2;
+    int cols = getOptimalDFTSize( img.cols )-1;
+    double w1 = -2*pi/cols;
+    double w2 = -2*pi/rows;
+
+    img_fast_fourier = Mat::zeros(rows, cols, CV_64F);
+
+    complex<double> **sum_cols = new complex<double> *[rows];
+    complex<double> **sum_rows = new complex<double> *[rows];
+    complex<double> **W = new complex<double> *[rows];
+    double **sum = new double *[rows];
+
+/*    for(i=0; i<rows; i++)
+    {
+        sum[i] = new double [cols];
+        W[i] = new complex<double> [cols];
+        for(j=0; j<cols; j++)
+        {
+            sum[i][j] = 0;
+            W[i][j] = 0;
+        }
+    }
+*/
+    for(i=0; i<rows; i++)
+    {
+        sum_cols[i] = new complex<double> [cols];
+        sum_rows[i] = new complex<double> [cols];
+        sum[i] = new double [cols];
+        W[i] = new complex<double> [cols];
+        for(j=0; j<cols; j++)
+        {
+            sum_cols[i][j] = 0;
+            sum_rows[i][j] = 0;
+            sum[i][j] = 0;
+            W[i][j] = 0;
+        }
+    }
+    for(i=0; i<rows/2; i++)
+    {
+        for(j=0; j<cols/2; j++)
+        {
+            for(N=0; N<cols/2; N++)
+            {
+                sum_cols[i][j] += complex<double>(img.at<uchar>(2*i,2*N),0)*complex<double>(cos(w1*2*j*N),sin(w1*2*j*N));//00
+                sum_cols[i][j+cols/2] += complex<double>(img.at<uchar>(2*i,2*N+1),0)*complex<double>(cos(w1*(2*j)*N),sin(w1*(2*j)*N));//01
+                sum_cols[i+rows/2][j] += complex<double>(img.at<uchar>(2*i+1,2*N),0)*complex<double>(cos(w1*(2*j+1)*N),sin(w1*(2*j+1)*N));//10
+                sum_cols[i+rows/2][j+cols/2] += complex<double>(img.at<uchar>(2*i+1,2*N+1),0)*complex<double>(cos(w1*(2*j+1)*N),sin(w1*(2*j+1)*N));//11
+            }
+        }
+    }
+    for(i=0; i<cols/2; i++)
+    {
+        for(j=0; j<rows/2; j++)
+        {
+            for(N=0; N<rows/2; N++)
+            {
+                sum_rows[j][i] += sum_cols[N][i]*complex<double>(cos(w2*2*j*N),sin(w2*2*j*N));//00
+                sum_rows[j][i+cols/2] += sum_cols[N][i+cols/2]*complex<double>(cos(w2*(2*j+1)*N),sin(w2*(2*j+1)*N));//01
+                sum_rows[j+rows/2][i] += sum_cols[N+rows/2][i]*complex<double>(cos(w2*(2*j)*N),sin(w2*(2*j)*N));//10
+                sum_rows[j+rows/2][i+cols/2] += sum_cols[N+rows/2][i+cols/2]*complex<double>(cos(w2*(2*j+1)*N),sin(w2*(2*j+1)*N));//11
+            }
+            W[j][i] = sum_rows[j][i]+sum_rows[j][i+cols/2]+sum_rows[j+rows/2][i]+sum_rows[j+rows/2][i+cols/2];
+            W[j+rows/2][i] = sum_rows[j][i]+sum_rows[j][i+cols/2]-sum_rows[j+rows/2][i]-sum_rows[j+rows/2][i+cols/2];
+            W[j][i+cols/2] = sum_rows[j][i]-sum_rows[j][i+cols/2]+sum_rows[j+rows/2][i]+sum_rows[j+rows/2][i+cols/2];
+            W[j+rows/2][i+cols/2] = sum_rows[j][i]-sum_rows[j][i+cols/2]-sum_rows[j+rows/2][i]-sum_rows[j+rows/2][i+cols/2];
+        }
+    }
+    for(i=0; i<cols; i++)
+    {
+        for(j=0; j<rows; j++)
+        {
+            sum[j][i] = (sqrt(pow((W[j][i].real()),2) + pow((W[j][i].imag()),2)));
+            img_fast_fourier.at<double>(j,i) = log(sum[j][i]+1);
+        }
+    }
+    beautiful_spectre(img_fast_fourier);
+  //  normalize(img_fast_fourier, img_fast_fourier, 0, 1, CV_MINMAX);
+    return img_fast_fourier;
+}
+
+
 void correct_work(Mat &img1, Mat &img2)
 {
     int Overlap=0;
@@ -197,5 +284,5 @@ void correct_work(Mat &img1, Mat &img2)
                 Overlap++;
         }
     }
-    cout<<"Correctness of work DFT = "<<Overlap*100/(img1.cols*img1.rows)<<" %"<<endl;
+    cout<<"Correctness of work = "<<Overlap*100/(img1.cols*img1.rows)<<" %"<<endl;
 }
