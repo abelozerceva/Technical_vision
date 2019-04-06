a00622900b5da08bb2c18cbfac7f262da1ec3a1b
belozercevaa <belozerceva.a@yandex.ru>
2019-04-06 22:42:46 +0300

    начало лабы с подсветкой

---
+++ Lab_6/main.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/aruco/charuco.hpp>
+#include <opencv2/aruco/dictionary.hpp>
+#include <opencv2/core/utility.hpp>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
